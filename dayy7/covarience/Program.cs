354689b54f8d1179b64681daf83f4f255dcb82b1
Eega-tejaswini <125671759+Eega-tejaswini@users.noreply.github.com>
2025-02-07 09:41:45 +0530

    Add files via upload

---
+++ dayy7/covarience/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+// See https://aka.ms/new-console-template for more information
+using Covarience;
+
+
+delegate Vehicle delveh();
+delegate void delcontra(Bike bike);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       
+
+        Vehicle vhl = new Vehicle();
+        delveh delv = new delveh(vhl.CreateVehicle);
+
+        Bike br = new Bike();
+
+        delv = new delveh(br.CreateBike);
+
+        //contravariance
+        delcontra delcont = new delcontra(vhl.Display);
+        delcont.Invoke(br);
+
+        delcont += br.DisplayBike;
+
+    }
+}
+
---
+++ dayy7/covarience/Vehicle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covarience
+{
+    internal class Vehicle
+    {
+        
+        
+            
+            public Vehicle CreateVehicle()
+            {
+                Console.WriteLine("Vehicle created");
+                return new Vehicle();
+            }
+         
+            public void Display(Vehicle vehobj)
+            {
+                Console.WriteLine("Vehicles class Method" + vehobj);
+            }
+
+        }
+        class Bike : Vehicle
+        {
+            public Bike CreateBike()
+            {
+                Console.WriteLine("BIKE Created");
+                return new Bike();
+            }
+            public void DisplayBike(Bike Bikeobj)
+            {
+                Console.WriteLine("bike class Method" + Bikeobj);
+            }
+        }
+    
+}
