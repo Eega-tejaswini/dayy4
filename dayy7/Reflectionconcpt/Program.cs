6f2bd160225e3f9f20de4618aa6d8ea2b910643f
Eega-tejaswini <125671759+Eega-tejaswini@users.noreply.github.com>
2025-02-07 09:51:03 +0530

    Add files via upload

---
+++ dayy7/Reflectionconcpt/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System.Reflection;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Type T = Type.GetType("ReflectionConcpt.Studentt");
+        Console.WriteLine(T.Name);
+        Console.WriteLine(T.FullName);
+
+
+
+
+        MethodInfo[] mthds = T.GetMethods();
+        Console.WriteLine(mthds.GetType);
+        Console.WriteLine(mthds.GetType().FullName);
+
+        foreach (MethodInfo method in mthds)
+        {
+            Console.WriteLine(method.ReturnType.Name + " " + method.Name);
+        }
+
+
+
+
+    }
+}
---
+++ dayy7/Reflectionconcpt/Studentt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflectionconcpt
+{
+    internal class Studentt
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Studentt()
+        {
+            Console.WriteLine(" default constructor");
+        }
+        public Studentt(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public void PrintId()
+        {
+            Console.WriteLine(" display " + Id);
+        }
+        public void PrintName()
+        {
+            Console.WriteLine(" display " + Name);
+        }
+    }
+}
