56f1fef968faa41d7e4141dfec3dc1b2b0ddda74
Eega-tejaswini <125671759+Eega-tejaswini@users.noreply.github.com>
2025-02-07 09:10:05 +0530

    Add files via upload

---
+++ dayy5/generic/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System.Xml.Linq;
+
+using Generic;
+{
+    List<string> courses = new List<string>();
+    courses.Add(".Net");
+    courses.Add("CSharp");
+    foreach (string course in courses)
+    {
+        Console.WriteLine(course);
+    }
+
+    List<int> Numbers = new List<int>
+
+}
+
