ff1fabb58f7700bcca69771e387e60882be792f4
Eega-tejaswini <125671759+Eega-tejaswini@users.noreply.github.com>
2025-02-07 09:27:33 +0530

    Add files via upload

---
+++ dayy5/xml sample/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xmlsample
+{
+   
+    
+        internal class Course
+        {
+            public int Cid { get; set; }
+            public string Name { get; set; }
+        }
+    
+}
+
