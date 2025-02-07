1d628990bafc13e1f7686adf147ca17446504c52
Eega-tejaswini <125671759+Eega-tejaswini@users.noreply.github.com>
2025-02-07 09:07:24 +0530

    Add files via upload

---
+++ dayy5/inheritancee/Interface1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuitipleInheritance
+{
+    internal interface Interface1
+    {
+        void BasicDet();
+        void SalDetails();
+     }
+    interface IMeeting
+        {
+            void AttendMeetings();
+        }
+    class PermanentEmployee : Interface1, IMeeting
+        {
+        public int empid { get; set; }
+        public string? empname { get; set; }
+        public decimal salary { get; set; }
+
+        public static string OrgName = "Wipro";  
+
+        public void AttendMeetings()
+        {
+            Console.WriteLine("Attend Meetings");
+        }
+        public void BasicDet()
+        {
+            Console.WriteLine(empid + " " + empname + " " + OrgName);
+        }
+
+        public void SalDetails()
+        {
+            Console.WriteLine(salary);
+        }
+    }
+    class TemporaryEmployee : Interface1
+    {
+        public int empid { get; set; }
+        public string? empname { get; set; }
+        public decimal salaryperday { get; set; }
+        public void BasicDet()
+        {
+            Console.WriteLine(empid + " " + empname);
+        }
+
+        public void SalDetails()
+        {
+            Console.WriteLine(salaryperday);
+        }
+
+
+    }
+}
---
+++ dayy5/inheritancee/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace MuitipleInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the type of employee 1.Permanent 2.Temporary");
+            string? type = Console.ReadLine();
+
+            if (type.ToLower().Contains("per"))
+            {
+                //  IEmployee employee = new PermanentEmployee() { empid = 111, empname = "Riya", salary = 70000 };
+                PermanentEmployee employee = new PermanentEmployee() { empid = 111, empname = "teju", salary = 70000 };
+                employee.BasicDet();
+                employee.SalDetails();
+                employee.AttendMeetings();
+                Interface1 emp = new PermanentEmployee() { empid = 113, empname = "vyshh", salary = 80000 };
+                emp.BasicDet();
+                emp.SalDetails();
+
+            }
+            else if (type.ToLower().Contains("temp"))
+            {
+                Interface1 tmp = new TemporaryEmployee()
+                {
+                    empid = 112,
+                    empname = "Riyaz",
+                    salaryperday = 7000
+                };
+                tmp.BasicDet();
+                tmp.SalDetails();
+            }
+
+        }
+    }
+}
