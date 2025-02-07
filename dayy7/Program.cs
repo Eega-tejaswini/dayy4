c3e68ae75b99e9a2c926b3cef6f10f6216744cfe
Eega-tejaswini <125671759+Eega-tejaswini@users.noreply.github.com>
2025-02-07 09:39:41 +0530

    Add files via upload

---
+++ dayy7/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+internal class Program
+{
+ 
+   // public delegate void delsample(string msg); 
+    public delegate void delsample2();
+    private static void Main(string[] args)
+    {
+      //  Program obj = new Program();
+
+     
+       // delsample del = new delsample(obj.Display);
+
+       // del("welcome");
+
+
+        delsample2 del2 = new delsample2(Display);
+        del2();
+
+        Display();
+
+
+    }
+    //Instance Method
+    void Display(string msg)
+    {
+        Console.WriteLine("Welcome to delegates" + msg);
+    }
+    //Static method
+    static void Display()
+    {
+        Console.WriteLine("Hello");
+    }
+}
