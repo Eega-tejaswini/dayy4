6aa670c1accd75ca0c1485dbf1426b2d6cedcd3d
Eega-tejaswini <125671759+Eega-tejaswini@users.noreply.github.com>
2025-02-07 09:53:20 +0530

    Add files via upload

---
+++ dayy7/partial class/Country.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClasses
+{
+
+        partial class Country
+        {
+            public static string Countryname = " india";
+            public void CountryMethod()
+            {
+                Console.WriteLine("Country is" + Countryname);
+            }
+        }
+        partial class Country
+        {
+            public static string ParentCountryNm = "India";
+
+            public void ParentCountryMethod()
+            {
+                Console.WriteLine("Country is" + Countryname);
+            }
+        }
+    
+}
---
+++ dayy7/partial class/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using PartialClasses;
+
+
+Country obj = new Country();
+obj.CountryMethod();
+obj.ParentCountryMethod();  
