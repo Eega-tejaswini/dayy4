627372e6baafe3503dcf38053b38e05a693e51bb
Eega-tejaswini <125671759+Eega-tejaswini@users.noreply.github.com>
2025-02-07 09:57:51 +0530

    Add files via upload

---
+++ dayy8/MathOp.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDemo
+{
+    
+  
+        public class MathOp
+        {
+            public virtual int Add(int a, int b)
+            {
+                return a + b;
+            }
+            public int Sub(int a, int b)
+            {
+                return a - b;
+            }
+            public int Mul(int a, int b)
+            {
+                return a * b;
+            }
+            public int Div(int a, int b)
+            {
+                return a / b;
+            }
+
+            public virtual bool CheckVal()
+            {
+                return false;
+            }
+
+        }
+        public class Student
+        {
+            string Name;
+            int Age;
+
+            public Student(string ename, int age)
+            {
+                Name = ename;
+                Age = age;
+            }
+            public string name
+            {
+                get
+                {
+                    return Name;
+                }
+                set
+                {
+                    Name = value;
+                }
+            }
+
+            public int age
+            {
+                get
+                {
+                    return Age;
+                }
+                set
+                {
+                    Age = value;
+                }
+            }
+
+            public void Display()
+            {
+                Console.WriteLine(Name + " " + Age);
+            }
+        }
+
+    
+}
+
