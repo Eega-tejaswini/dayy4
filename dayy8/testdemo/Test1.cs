0a52a95b5b085f4a62da3e0f26921c3c54796dd9
Eega-tejaswini <125671759+Eega-tejaswini@users.noreply.github.com>
2025-02-07 10:00:42 +0530

    Add files via upload

---
+++ dayy8/testdemo/Test1.cs
@@ -0,0 +1,35 @@
+﻿using SampleDemo;
+
+namespace TestProjectMs
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethodAdd()
+        {
+            MathOp mth = new MathOp();
+            int res = mth.Add(30, 30);
+            Assert.AreEqual(60,res);
+        }
+
+        [TestMethod]
+        public void TestMethodSub()
+        {
+            //test for Add method
+            MathOp mth = new MathOp();
+            int res = mth.Sub(50, 20);
+            Assert.AreEqual(res, 40);
+        }
+
+        [TestMethod]
+
+
+        public void TestMethodProd()
+        {
+            MathOp mth = new MathOp();
+            int res = mth.Mul(5, 4);
+            Assert.AreEqual(res, 20);
+        }
+    }
+}
