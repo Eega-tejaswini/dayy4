21d825b9b367173dee09e76d80e8da583ae9a5fe
Eega-tejaswini <125671759+Eega-tejaswini@users.noreply.github.com>
2025-02-07 09:43:56 +0530

    Add files via upload

---
+++ dayy7/delegateusages/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static DeleUsage.Student;
+
+Console.WriteLine("Hello, World!");//
+
+
+namespace DeleUsage
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student() { stdid = 1, name = " vyshh", marks = 60 },
+                new Student() { stdid = 2, name = " gythri", marks = 50 },
+                new Student() { stdid = 3, name = " teju", marks = 55 }
+            };
+            Delpromote delpromote = new Delpromote(IsPromote);
+            Student.Promotestudent(students, delpromote);
+        }
+
+        static bool IsPromote(Student st)
+        {
+          if (st.marks > 50)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+} 
+
---
+++ dayy7/delegateusages/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleUsage
+{
+    
+    
+        public delegate bool Delpromote(Student st);
+        public class Student
+        {
+            public int stdid { get; set; }
+            public string name { get; set; }    
+            public int marks { get; set; }
+
+            public static void Promotestudent(List<Student> lst , Delpromote isPromotable)
+            {
+                foreach (Student st in lst) 
+                {
+                    if (isPromotable(st))
+                    {
+                        Console.WriteLine(st.name + " is promoted");
+                    }
+                }
+            }
+        }
+    
+}
