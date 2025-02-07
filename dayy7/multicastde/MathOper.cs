7821de07d1d8e1fce3c13ab5991e0d44d3e857db
Eega-tejaswini <125671759+Eega-tejaswini@users.noreply.github.com>
2025-02-07 09:49:30 +0530

    Add files via upload

---
+++ dayy7/multicastde/MathOper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiCastDe
+{
+    public delegate void MathOpDel(int c, int d);
+    internal class MathOper
+    {
+       // public delegate void MathOpDel(int c, int d);
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static void Sub(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+        public static void Mul(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+        public static void Div(int a, int b)
+        {
+            Console.WriteLine(a / b);
+        }
+    }
+}
---
+++ dayy7/multicastde/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using MultiCastDe;
+
+MathOpDel mth = new MathOpDel(MathOper.Add);
+
+mth.Invoke(10, 20); 
+mth(30, 20);
+
+mth += MathOper.Sub;
+mth += MathOper.Mul; 
+mth += MathOper.Div;
+mth(50, 50);
+
+mth -= MathOper.Div;
+mth(50, 60);
+
+
+// function Delegates
+Func<int, int, int, int> fundel = delegate (int x, int y, int z)
+{
+    return x + y + z;
+};
+Console.WriteLine(fundel(10, 20, 30));
+
+
+Func<int, int> fundellam = x => x * x;
+Console.WriteLine(fundellam(20));
+
+// action delegates
+
+Action<int, int> actiondel = (x, y) => Console.WriteLine(x + y);
+
+actiondel(10, 20);
+
+//Predicate delegates
+
+
+Predicate<int> isEven = (num) => num % 2 == 0;
+
+if (isEven(10)) 
+{
+    Console.WriteLine("Num value is even");
+}
+else
+{
+    Console.WriteLine("Num values is odd");
+}
